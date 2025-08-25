8d57bb1a94659a96d0d16dcd4ce86781df84cd63
gustavolimapereira <gu.lim@hotmail.com>
2025-08-25 17:24:33 -0300

    configuração para token expirar depois de 1 dia

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -20,6 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: Buffer.from(publicKey, 'base64'),
       algorithms: ['RS256'],
+      expiresIn: '1d',
     })
   }
 
